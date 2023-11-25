5e6bfe25b1cb2901ecaa9c0b197ac1520dde34be
Kemjiga <kemerj@uninorte.edu.co>
2023-11-24 20:26:14 -0500

    fix: change user login with jwt

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -5,12 +5,13 @@ import {
   getUserById,
   updateUser,
   deleteUser,
+  login,
 } from '../controllers/user.controller';
 
 const router = Router();
 
 router.post('/create/', createUser);
-router.post('/', getUser);
+router.post('/', login);
 router.get('/:id', getUserById);
 router.patch('/:id', updateUser);
 router.delete('/:id', deleteUser);
